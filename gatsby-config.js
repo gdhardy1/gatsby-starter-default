396609bdb078ecfa6afdef02becdb6fcabbef1e7
gdhardy1 <gdhardy1@gmail.com>
2021-05-13 08:34:16 -0400

    hard code prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: `/${process.env.TEST_PREFIX}`,
+  pathPrefix: `/g`,
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
