ec6b15b94b7e420d77b92b2670ed2cd7398db981
gdhardy1 <gdhardy1@gmail.com>
2021-05-13 08:17:13 -0400

    path prefix

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,12 +5,3 @@
  */
 
 // You can delete this file if you're not using it
-exports.createPages = ({ graphql, actions }) => {
-  const { createRedirect } = actions
-  createRedirect({
-    fromPath: "https://gatsbystarterdefault65297.gatsbyjs.io/*",
-    toPath: "https://gregorydhardy.com/*",
-    isPermanent: true,
-  })
-  // Create pages here
-}
